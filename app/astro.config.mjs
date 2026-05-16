8fdb338a7ae59747096c8566c06d9aeeb7cd5945
Michal Szabados <misko.sz@gmail.com>
2026-05-16 16:50:06 +0200

    Callouts, RRS definition

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import { rehypeRrs } from "./src/lib/rehype-rrs.ts";
 import { rehypeGithubAlerts } from "rehype-github-alerts";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   site: "https://miskosz.github.io",
@@ -10,6 +12,7 @@ export default defineConfig({
   devToolbar: { enabled: false },
   markdown: {
     syntaxHighlight: false,
-    rehypePlugins: [rehypeRrs, rehypeGithubAlerts],
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeRrs, rehypeGithubAlerts, rehypeKatex],
   },
 });
